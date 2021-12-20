83cc9813d608e4475474b879d4fd33bb2840bd99
Phạm Thị Lan Anh <90228386+LananhHoadanhdanh@users.noreply.github.com>
2021-12-20 11:30:54 +0700

    cấu hình chuẩn

--- src/main/java/service/like/ILikeService.java
+++ src/main/java/service/like/ILikeService.java
@@ -7,9 +7,9 @@ import service.IGeneralService;
 import java.util.Optional;
 
 public interface ILikeService extends IGeneralService<LikePost> {
-    @Query("select l from LikePost l where l.postId.id=:idP and l.user.id=:idUs")
+    @Query("select l from LikePost l where l.post.id=:idP and l.user.id=:idUs")
     public Optional<LikePost> findAllByPostIdAndUserId(Long idP, Long idUs);
 
-    @Query("select l from LikePost  l where l.postId.id=:id")
+    @Query("select l from LikePost  l where l.post.id=:id")
     public Iterable<LikePost> findAllByPostId(Long id);
 }
