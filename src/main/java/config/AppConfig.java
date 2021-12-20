52fe3ab45fe43607cfe022eee388b99e0d7ac106
Phạm Thị Lan Anh <90228386+LananhHoadanhdanh@users.noreply.github.com>
2021-12-20 09:39:40 +0700

    project

--- src/main/java/config/AppConfig.java
+++ src/main/java/config/AppConfig.java
@@ -107,7 +107,7 @@ public class AppConfig implements WebMvcConfigurer, ApplicationContextAware {
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver"); // loại driver đang dùng
-        dataSource.setUrl("jdbc:mysql://localhost:3306/thi_thuc_hanh_module_3"); // csdl đang dùng
+        dataSource.setUrl("jdbc:mysql://localhost:3306/social_web"); // csdl đang dùng
         dataSource.setUsername("root"); // tài khoản sql
         dataSource.setPassword("123456"); // mật khẩu sql
         return dataSource;
